8e031ba1837c7bf85716c268af83173b690e1482
rkeerthick <rkeerthick1@gmail.com>
2023-07-20 22:32:45 +0530

    global clients completed

--- demo.js
+++ demo.js
@@ -1,6 +1,14 @@
 const gallery = {1:"/images/gallery/Social Networks .jpg", 2:"/images/gallery/Supply Chain.jpg", 3:"/images/gallery/Media 003.jpg", 4:"/images/gallery/Fintech 004.jpg", 5:"/images/gallery/Health Tech.jpg",  6:"/images/gallery/E-commerce.jpg", 7:"/images/gallery/Travel Tech.jpg", 8:"/images/gallery/Edutech.jpg"}
 
-// console.log(gallery[1])
+let size = 1
+for(let i in gallery) {
+    setTimeout(() => {
+        document.getElementById("client-gallery").src = gallery[size]
+        size++
+    }, 5000)
+    
+    
+}
 
 function display(list) {
     console.log(gallery[list.value]);
